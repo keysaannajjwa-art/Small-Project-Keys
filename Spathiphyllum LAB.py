3476a146ff8da2e199bb886ee147adbd090a309c
keysaannajjwa-art <keysaannajjwa@gmail.com>
2026-04-26 16:53:36 +0700

    practice on LAB cisco academy
    my practice on LAB module cisco academy, print someting with if and keyword in variable

---
+++ Spathiphyllum LAB.py
@@ -0,0 +1,13 @@
+#jika input Spathiphyllum maka balesannya "Yes - Spathiphyllum is the best plant ever!" 
+#jika inputnya tanaman yang lain balesannya "Spathiphyllum not (input)"
+#TAPI kalo spathiphyllum nya S nya LOWERCASE balasannya " No, i want a big Spathiphyllum!"
+
+PlanT = input()
+
+if PlanT == "Spathiphyllum":
+	print ("Yes, Spathiphyllum is the best plan ever!")
+elif PlanT == "spathiphyllum":
+	print("No, i want big Spathiphyllum")
+else: print("Spathiphyllum not", PlanT)
+	
+	#3.1.10   LAB   Comparison operators and conditional execution
